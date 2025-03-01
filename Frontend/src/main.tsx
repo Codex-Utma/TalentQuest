a2efbf1d1a4ad87abe8f1c66bd46f26577605356
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2025-02-28 18:11:03 -0600

    feat: implement routing with React Router for multiple pages and reorganize folder structure for better maintainability

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
