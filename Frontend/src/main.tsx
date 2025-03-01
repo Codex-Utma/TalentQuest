ddc2ac4cea805ba6a30c1f3a5c5af28412e1aee9
Maricruz1900 <utm22030695@utma.edu.mx>
2025-02-28 19:54:59 -0600

    Ruta de prueba

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.tsx'
+import Prueba from './pages/prueba.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/prueba' element={<Prueba />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ Frontend/src/pages/prueba.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+        <h1>Prueba</h1>
+      <p className="read-the-docs">
+        Click on the Vite ndhducfdgvhsdbjwdhwjdejh
+      </p>
+    </>
+  )
+}
+
+export default App
