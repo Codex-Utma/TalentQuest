6520a5c3edff3721e5055bacec06c45a9549f1fc
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2025-03-01 22:43:52 -0600

    feat:  Implemented a learning quiz

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-export default App;
