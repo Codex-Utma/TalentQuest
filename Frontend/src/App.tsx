c81e72479d8abff0116ea389b641784179a7b6db
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2025-03-02 20:49:54 -0600

    feat:The structure of the administrator page is added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Questionnaire from "./pages/questionnaire";
 import './index.css';
+import AdministratorPage from "./pages/administrator";
 
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/questionnaire" element={<Questionnaire/>} />
+      <Route path="/administrator" element={<AdministratorPage/>} />
     </Routes>
   </BrowserRouter>
 );
